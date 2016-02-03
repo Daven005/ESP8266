0d9307975ac5089da9e9dd40188d75a0d109bef2
Daven005 <david@norburys.me.uk>
2016-02-03 11:08:55 +0000

    V 1.0.3.2 Added IP address to deviceInfo msg

---
+++ TemperatureMonitor/include/timezone.h
@@ -0,0 +1,13 @@
+/*
+ * timezone.h
+ *
+ *  Created on: 22 Sep 2015
+ *      Author: User
+ */
+
+#ifndef INCLUDE_TIMEZONE_H_
+#define INCLUDE_TIMEZONE_H_
+
+void applyDST(struct tm *time);
+
+#endif /* INCLUDE_TIMEZONE_H_ */
